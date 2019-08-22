d862aa9ce4ded20f0f57b7ffe893299236e1840e
Juan Martín Loyola <jmloyola@outlook.com>
2019-08-22 18:20:31 -0300

    Remove unused importts

--- ClassificationScripts/experimental_vs_theoretical_classification.py
+++ ClassificationScripts/experimental_vs_theoretical_classification.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import numpy as np
-import scipy as sp
 
 from sklearn import preprocessing
-from sklearn.preprocessing import OneHotEncoder
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_recall_fscore_support
